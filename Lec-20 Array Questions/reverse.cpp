6ffc3f1b55271af6dec20cebb88c55a3c69f2372
coderyogesh <yogeshrj1433@gmail.com>
2023-09-13 21:14:46 +0530

    Reverse the array from a specified index.

---
+++ Lec-20 Array Questions/reverse.cpp
@@ -0,0 +1,14 @@
+// https://www.codingninjas.com/studio/problems/reverse-the-array_1262298
+
+#include <bits/stdc++.h>
+void reverseArray(vector<int> &arr, int m)
+{
+	int s = m + 1, e = arr.size() - 1;
+
+	while (s <= e)
+	{
+		swap(arr[s], arr[e]);
+		s++;
+		e--;
+	}
+}

8c13c75cb98bab8879218a922dc4a93b0d85c119
coderyogesh <yogeshrj1433@gmail.com>
2023-09-17 12:50:13 +0530

    Introduction to 2-D Arrays

---
+++ Lec-23 2-D Arrays Intro/intro.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+bool isPresent(int arr[][4], int target, int row, int col)
+{
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            if (arr[row][col] == target)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int arr[3][4];
+    // int arr[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    // int arr[3][4] = {{1, 11, 111, 1111}, {2, 22, 222, 2222}, {3, 33, 333, 3333}};
+    // Printing row - wise
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cin >> arr[row][col];
+        }
+    }
+
+    // col wise input
+    // for (int row = 0; row < 4; row++)
+    // {
+    //     for (int col = 0; col < 3; col++)
+    //     {
+    //         cin >> arr[col][row];
+    //     }
+    // }
+    // printing
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cout << arr[row][col] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Enter the element to search" << endl;
+    int target;
+    cin >> target;
+
+    if (isPresent(arr, target, 3, 4))
+    {
+        cout << "Element Found" << endl;
+    }
+    else
+    {
+        cout << "Not Found" << endl;
+    }
+    return 0;
+}

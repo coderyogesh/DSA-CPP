c0de223870eed2abee59d34f5a941fbeb1c80738
coderyogesh <yogeshrj1433@gmail.com>
2023-09-19 00:13:05 +0530

    Print Array like a Wave

---
+++ Lec-23 2-D Arrays Intro/printWave.cpp
@@ -0,0 +1,33 @@
+// https://www.codingninjas.com/studio/problems/print-like-a-wave_893268
+// Print Like A Wave
+
+#include <bits/stdc++.h>
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    vector<int> ans;
+
+    for (int col = 0; col < mCols; col++)
+    {
+
+        if (col & 1)
+        {
+            // odd Index -> Bottom to top
+
+            for (int row = nRows - 1; row >= 0; row--)
+            {
+                // cout << arr[row][col] <<" ";
+                ans.push_back(arr[row][col]);
+            }
+        }
+        else
+        {
+            // 0 or even iondex -> top to bottom
+            for (int row = 0; row < nRows; row++)
+            {
+                // cout << arr[row][col] << " ";
+                ans.push_back(arr[row][col]);
+            }
+        }
+    }
+    return ans;
+}

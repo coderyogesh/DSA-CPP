914c3346c55c6a47b09de06bdee9eaea9c1b09c3
coderyogesh <yogeshrj1433@gmail.com>
2023-09-21 13:32:45 +0530

    count primes

---
+++ Lec-24 Basic Maths for DSA || Sieve || Modular Arithmetics || Euclid’s Algorithm/countPrimes.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/count-primes/
+// 204. Count Primes
+// Medium level prblem
+
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+
+        int cnt = 0;
+        vector<bool> prime(n + 1, true);
+
+        prime[0] = prime[1] = false;
+
+        for (int i = 2; i < n; i++)
+        {
+            if (prime[i])
+            {
+                cnt++;
+
+                for (int j = 2 * i; j < n; j = j + i)
+                {
+                    prime[j] = 0;
+                }
+            }
+        }
+        return cnt;
+    }
+};

d7acf9f7871daee3903da4312e3894e7227aa4e2
coderyogesh <yogeshrj1433@gmail.com>
2023-09-22 11:42:38 +0530

    Modular Exponentiation

---
+++ Lec-24 Basic Maths for DSA || Sieve || Modular Arithmetics || Euclid’s Algorithm/modularExponentiation.cpp
@@ -0,0 +1,22 @@
+// https://www.codingninjas.com/studio/problems/modular-exponentiation_1082146
+// Medium level problem
+
+#include <bits/stdc++.h>
+
+int modularExponentiation(int x, int n, int m)
+{
+    int res = 1;
+
+    while (n > 0)
+    {
+
+        if (n & 1)
+        {
+            // odd
+            res = (1LL * (res) * (x) % m) % m;
+        }
+        x = (1LL * (x) % m * (x) % m) % m;
+        n = n >> 1;
+    }
+    return res;
+}
